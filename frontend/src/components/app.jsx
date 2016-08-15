495cb47945016321b1cf71e24b45aaf4a2ecf1b1
Lawrence Wakefield <lawrence@iamlawrence.me>
2016-08-15 12:11:45 -0400

    - added overlay pre-login

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -7,6 +7,7 @@ import styles from './styles'
 
 import Api from 'src/api'
 import Todos from 'src/todos'
+import Auth from 'src/auth'
 
 export default class App extends Component {
   constructor () {
@@ -15,6 +16,7 @@ export default class App extends Component {
   }
   componentWillMount () {
     Todos.subscribe((k, v) => { this.setState(v) })
+    Auth.subscribe((k, v) => { this.setState(v) })
   }
   render () {
     let tasksRemaining = this.state.todos.filter(v => !v.completed).length
@@ -22,19 +24,27 @@ export default class App extends Component {
       <div>
         <TopBar />
         <div class={styles.app}>
-          <header class={styles.header}>
-            <h1>Todos</h1>
-          </header>
-          <NewTodoForm/>
-          <TodoList/>
-          <footer class={styles.footer}>
-            <span>{tasksRemaining} items left</span>
-            <button class={styles.button}
-              onClick={() => this.completeAll()}>Mark all as complete</button>
-          </footer>
+          <div class={!this.state.isAuthed ? styles.blur : ''}>
+            <header class={styles.header}>
+              <h1>Todos</h1>
+            </header>
+            <NewTodoForm/>
+            <TodoList/>
+            <footer class={styles.footer}>
+              <span>{tasksRemaining} items left</span>
+              <button class={styles.button}
+                onClick={() => this.completeAll()}>Mark all as complete</button>
+            </footer>
+          </div>
+          { this.renderOverlay() }
         </div>
       </div>
     )
+  }
+  renderOverlay () {
+    if (this.state.isAuthed) return null
+
+    return <div class={styles.overlay}>Login to get started</div>
   }
   completeAll () {
     Api.completeAll()
